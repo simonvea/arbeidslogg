4c8a8572354d1e47ecd3e486aeefc11781c7cc8a
Simon Opheim <simon.opheim@gmail.com>
2019-06-04 22:57:34 +0200

    removed log inn button

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="row justify-content-center">
             <div class="btn-group" role="toolbar" aria-label="toolbar">
-                <button type="button" class="btn btn-secondary">Logg inn / ut</button>
                 <button type="button" class="btn btn-danger" v-if="checkedIn" v-on:click="$emit('check-out')">Sjekk ut</button>
                 <button type="button" class="btn btn-success" v-else v-on:click="$emit('check-in')">Sjekk inn</button>
                 <button type="button" class="btn btn-secondary" v-on:click="$emit('new-task')">Ny oppgave</button>
